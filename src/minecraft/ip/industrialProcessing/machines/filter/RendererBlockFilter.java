25e21b54d24b5e9407b2b6795886573603a9fb90
Tobias VdP <tobiasvdp@outlook.com>
2013-08-06 23:24:19 +0200

    added RendererBlockFilter

---
+++ src/minecraft/ip/industrialProcessing/machines/filter/RendererBlockFilter.java
@@ -0,0 +1,71 @@
+package ip.industrialProcessing.machines.filter;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+
+public class RendererBlockFilter implements ISimpleBlockRenderingHandler{
+	final int renderID;
+
+
+    public RendererBlockFilter(int var1)
+    {
+        this.renderID = var1;
+    }
+
+
+    public void render(RenderBlocks renderBlocks, IBlockAccess iblockaccess, Block par1Block, int par2, int par3, int par4)
+    {
+        renderBlocks.overrideBlockTexture = par1Block.getBlockTexture(iblockaccess, par2, par3, par4, 0);
+
+
+        renderBlocks.setRenderBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.92F, 1.0F);
+        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+
+
+        renderBlocks.clearOverrideBlockTexture();
+    }
+
+
+    private final TileEntityFilter table = new TileEntityFilter();
+
+
+    @Override
+    public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer)
+    {
+        GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
+        GL11.glTranslatef(-0.5F, -1.1F, -0.1F);
+        GL11.glScalef(0.7F, 0.6F, 0.7F);
+        TileEntityRenderer.instance.renderTileEntityAt(table, 0.0D, 0.0D, 0.0D, 0.0F);
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+    }
+
+
+    @Override
+    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
+    {
+        this.render(renderer, world, block, x, y, z);
+        return true;
+    }
+
+
+    @Override
+    public boolean shouldRender3DInInventory()
+    {
+        return true;
+    }
+
+
+    @Override
+    public int getRenderId()
+    {
+        return this.renderID;
+    }
+
+}
