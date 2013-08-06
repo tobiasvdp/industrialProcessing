8b398861605cb721fd52f4f782e8ea854f83a685
Stijn Tallon <stijn_tallon@msn.com>
2013-08-06 23:04:42 +0200

    Commit ALL files you create or don't commit references to things you don't commit!

--- src/minecraft/ip/industrialProcessing/client/ClientProxy.java
+++ src/minecraft/ip/industrialProcessing/client/ClientProxy.java
@@ -6,7 +6,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.client.MinecraftForgeClient;
 import ip.industrialProcessing.CommonProxy;
 import ip.industrialProcessing.IndustrialProcessing;
-import ip.industrialProcessing.machines.filter.RendererBlockFilter;
+//import ip.industrialProcessing.machines.filter.RendererBlockFilter;
 import ip.industrialProcessing.machines.filter.RendererFilter;
 import ip.industrialProcessing.machines.filter.TileEntityFilter;
 
@@ -18,7 +18,7 @@ public class ClientProxy extends CommonProxy {
     public void registerRenderers() {
     	ClientRegistry.bindTileEntitySpecialRenderer(TileEntityFilter.class, new RendererFilter());
     	ClientProxy.FilterID = RenderingRegistry.getNextAvailableRenderId();
-        RenderingRegistry.registerBlockHandler(new RendererBlockFilter(ClientProxy.FilterID));
+        //RenderingRegistry.registerBlockHandler(new RendererBlockFilter(ClientProxy.FilterID));
 
     }
 	public static int getFilterID(){return FilterID;}
