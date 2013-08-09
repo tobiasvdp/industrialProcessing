9b9085b06952904a8c62a3dd262fb043089c953d
Stino <stijn_tallon@msn.com>
2013-08-09 18:23:07 +0200

    Mixer Fluid rendering Crusher Model

--- src/minecraft/ip/industrialProcessing/machines/Model.java
+++ src/minecraft/ip/industrialProcessing/machines/Model.java
@@ -1,8 +1,12 @@
 package ip.industrialProcessing.machines;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.util.Icon;
 
 public abstract class Model extends ModelBase{  
 	public abstract void renderModel(float f) ;
 	public abstract void renderModelAnimated(float f, float progress);
+	public void renderLiquid(float f, int tankSlot, float percentageFilled, Icon icon){
+		// Do Nothing by default
+	}
 }
