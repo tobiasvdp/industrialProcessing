9445a843e6dcf194bdfa84cda9095e11a0606d56
Tobias VdP <tobiasvdp@outlook.com>
2013-08-13 16:54:43 +0200

    Added pipe logic to the blast furnace. Still no item saving.

--- src/minecraft/ip/industrialProcessing/machines/TileEntityMachine.java
+++ src/minecraft/ip/industrialProcessing/machines/TileEntityMachine.java
@@ -34,252 +34,254 @@ import net.minecraftforge.common.ForgeDirection;
 
 public abstract class TileEntityMachine extends TileEntitySynced implements ISidedInventory, IMachineSlots {
 
-    private ArrayList<MachineItemStack> itemStacks = new ArrayList<MachineItemStack>();
-    private int[][] itemStackSideSlots = new int[6][0]; 
-    public boolean isDummyBlock = false;
+	private ArrayList<MachineItemStack> itemStacks = new ArrayList<MachineItemStack>();
+	private int[][] itemStackSideSlots = new int[6][0];
+	public boolean isDummyBlock = false;
 
-     public ForgeDirection getForwardDirection(){
-	 int meta = this.worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
-	 return BlockMachine.getForwardFromMetadata(meta);
-     }
-    
-
-
-    @Override
-    public void writeToNBT(NBTTagCompound nbt) {
-	super.writeToNBT(nbt);
-	writeInventory(nbt); 
-    } 
-
-    private void writeInventory(NBTTagCompound nbt) {
-	NBTTagList nbttaglist = new NBTTagList();
-	for (int i = 0; i < this.itemStacks.size(); ++i) {
-	    MachineItemStack machineStack = this.itemStacks.get(i);
-	    if (machineStack.stack != null) {
-		NBTTagCompound nbttagcompound1 = new NBTTagCompound();
-		nbttagcompound1.setByte("Slot", (byte) i);
-		machineStack.stack.writeToNBT(nbttagcompound1);
-		nbttaglist.appendTag(nbttagcompound1);
-	    }
+	public ForgeDirection getForwardDirection() {
+		int meta = this.worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
+		return BlockMachine.getForwardFromMetadata(meta);
 	}
-	nbt.setTag("Items", nbttaglist);
-    }
 
-    @Override
-    public void readFromNBT(NBTTagCompound nbt) {
-	super.readFromNBT(nbt);
-	readInventory(nbt);
-    };
-
-    private void readInventory(NBTTagCompound nbt) {
-	NBTTagList nbttaglist = nbt.getTagList("Items");
-	for (int i = 0; i < nbttaglist.tagCount(); ++i) {
-	    NBTTagCompound nbttagcompound1 = (NBTTagCompound) nbttaglist.tagAt(i);
-	    byte b0 = nbttagcompound1.getByte("Slot");
-
-	    if (b0 >= 0 && b0 < itemStacks.size()) {
-		MachineItemStack machineStack = this.itemStacks.get(b0);
-		machineStack.stack = ItemStack.loadItemStackFromNBT(nbttagcompound1);
-	    }
+	@Override
+	public void writeToNBT(NBTTagCompound nbt) {
+		super.writeToNBT(nbt);
+		writeInventory(nbt);
 	}
-    }
 
-    @Override
-    public boolean removeFromSlot(int slot, int itemId, int amount) {
-	if (slotContains(slot, itemId, amount)) {
-	    MachineItemStack machineStack = itemStacks.get(slot);
-	    machineStack.stack.stackSize -= amount;
-	    if (machineStack.stack.stackSize == 0)
+	private void writeInventory(NBTTagCompound nbt) {
+		NBTTagList nbttaglist = new NBTTagList();
+		for (int i = 0; i < this.itemStacks.size(); ++i) {
+			MachineItemStack machineStack = this.itemStacks.get(i);
+			if (machineStack.stack != null) {
+				NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+				nbttagcompound1.setByte("Slot", (byte) i);
+				machineStack.stack.writeToNBT(nbttagcompound1);
+				nbttaglist.appendTag(nbttagcompound1);
+			}
+		}
+		nbt.setTag("Items", nbttaglist);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+		readInventory(nbt);
+	};
+
+	private void readInventory(NBTTagCompound nbt) {
+		NBTTagList nbttaglist = nbt.getTagList("Items");
+		for (int i = 0; i < nbttaglist.tagCount(); ++i) {
+			NBTTagCompound nbttagcompound1 = (NBTTagCompound) nbttaglist.tagAt(i);
+			byte b0 = nbttagcompound1.getByte("Slot");
+
+			if (b0 >= 0 && b0 < itemStacks.size()) {
+				MachineItemStack machineStack = this.itemStacks.get(b0);
+				machineStack.stack = ItemStack.loadItemStackFromNBT(nbttagcompound1);
+			}
+		}
+	}
+
+	@Override
+	public boolean removeFromSlot(int slot, int itemId, int amount) {
+		if (slotContains(slot, itemId, amount)) {
+			MachineItemStack machineStack = itemStacks.get(slot);
+			machineStack.stack.stackSize -= amount;
+			if (machineStack.stack.stackSize == 0)
+				machineStack.stack = null;
+			onInventoryChanged();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean addToSlot(int slot, int itemId, int amount) {
+		if (slotHasRoomFor(slot, itemId, amount)) {
+			MachineItemStack machineStack = itemStacks.get(slot);
+			if (machineStack.stack == null)
+				machineStack.stack = new ItemStack(itemId, amount, 0);
+			else
+				machineStack.stack.stackSize += amount;
+			onInventoryChanged();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean slotContains(int slot, int itemId, int amount) {
+		MachineItemStack machineStack = itemStacks.get(slot);
+		return machineStack != null && machineStack.stack != null && machineStack.stack.itemID == itemId && machineStack.stack.stackSize >= amount;
+	}
+
+	@Override
+	public boolean slotHasRoomFor(int slot, ItemStack stack) {
+		if (stack == null || stack.stackSize == 0)
+			return true;
+		MachineItemStack machineStack = itemStacks.get(slot);
+		return machineStack != null && (machineStack.stack == null || (machineStack.stack.itemID == stack.itemID && (machineStack.stack.stackSize + stack.stackSize < getInventoryStackLimit())));
+	}
+
+	@Override
+	public boolean slotHasRoomFor(int slot, int itemId, int amount) {
+		if (amount == 0)
+			return true;
+		MachineItemStack machineStack = itemStacks.get(slot);
+		return machineStack != null && (machineStack.stack == null || (machineStack.stack.itemID == itemId && (machineStack.stack.stackSize + amount < getInventoryStackLimit())));
+	}
+
+	protected void addStack(ItemStack stack, LocalDirection side, boolean input, boolean output) {
+		int index = itemStacks.size();
+		addStack(stack, new LocalDirection[] { side }, input, output);
+	}
+
+	protected void addStack(ItemStack stack, LocalDirection[] sides, boolean input, boolean output) {
+		int index = itemStacks.size();
+		int[] sideIndices = new int[sides.length];
+		for (int i = 0; i < sideIndices.length; i++) {
+			sideIndices[i] = sides[i].ordinal();
+		}
+
+		itemStacks.add(new MachineItemStack(stack, sideIndices, input, output));
+
+		for (int i = 0; i < sideIndices.length; i++) {
+			int sideIndex = sideIndices[i];
+			int[] slots = itemStackSideSlots[sideIndex];
+			int[] newSlots = new int[slots.length + 1];
+			System.arraycopy(slots, 0, newSlots, 0, slots.length);
+			newSlots[slots.length] = index;
+			itemStackSideSlots[sideIndex] = newSlots;
+		}
+	}
+
+	@Override
+	public int getSizeInventory() {
+		return itemStacks.size();
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int i) {
+		MachineItemStack machineStack = getMachineStack(i);
+		if (machineStack == null)
+			return null;
+		return machineStack.stack;
+	}
+
+	protected MachineItemStack getMachineStack(int i) {
+		if (i < 0 || i >= this.itemStacks.size())
+			return null;
+		return this.itemStacks.get(i);
+	}
+
+	@Override
+	public ItemStack decrStackSize(int i, int j) {
+		MachineItemStack machineStack = getMachineStack(i);
+		if (machineStack == null)
+			return null;
+
+		ItemStack stack = machineStack.stack;
+		if (stack == null)
+			return null;
+
+		if (stack.stackSize > j) {
+			stack = stack.splitStack(j);
+			onInventoryChanged();
+			return stack;
+		}
 		machineStack.stack = null;
-	    onInventoryChanged();
-	    return true;
-	}
-	return false;
-    }
-
-    @Override
-    public boolean addToSlot(int slot, int itemId, int amount) {
-	if (slotHasRoomFor(slot, itemId, amount)) {
-	    MachineItemStack machineStack = itemStacks.get(slot);
-	    if (machineStack.stack == null)
-		machineStack.stack = new ItemStack(itemId, amount, 0);
-	    else
-		machineStack.stack.stackSize += amount;
-	    onInventoryChanged();
-	    return true;
-	}
-	return false;
-    }
-
-    @Override
-    public boolean slotContains(int slot, int itemId, int amount) {
-	MachineItemStack machineStack = itemStacks.get(slot);
-	return machineStack != null && machineStack.stack != null && machineStack.stack.itemID == itemId && machineStack.stack.stackSize >= amount;
-    }
-
-    @Override
-    public boolean slotHasRoomFor(int slot, ItemStack stack) {
-	if (stack == null || stack.stackSize == 0)
-	    return true;
-	MachineItemStack machineStack = itemStacks.get(slot);
-	return machineStack != null && (machineStack.stack == null || (machineStack.stack.itemID == stack.itemID && (machineStack.stack.stackSize + stack.stackSize < getInventoryStackLimit())));
-    }
-
-    @Override
-    public boolean slotHasRoomFor(int slot, int itemId, int amount) {
-	if (amount == 0)
-	    return true;
-	MachineItemStack machineStack = itemStacks.get(slot);
-	return machineStack != null && (machineStack.stack == null || (machineStack.stack.itemID == itemId && (machineStack.stack.stackSize + amount < getInventoryStackLimit())));
-    }
-
-    protected void addStack(ItemStack stack, LocalDirection side, boolean input, boolean output) {
-	int index = itemStacks.size();
-	addStack(stack, new LocalDirection[] { side }, input, output);
-    }
-
-    protected void addStack(ItemStack stack, LocalDirection[] sides, boolean input, boolean output) {
-	int index = itemStacks.size();
-	int[] sideIndices = new int[sides.length];
-	for (int i = 0; i < sideIndices.length; i++) { 
-	    sideIndices[i] = sides[i].ordinal();
+		onInventoryChanged();
+		return stack;
 	}
 
-	itemStacks.add(new MachineItemStack(stack, sideIndices, input, output));
-
-	for (int i = 0; i < sideIndices.length; i++) {
-	    int sideIndex = sideIndices[i];
-	    int[] slots = itemStackSideSlots[sideIndex];
-	    int[] newSlots = new int[slots.length + 1];
-	    System.arraycopy(slots, 0, newSlots, 0, slots.length);
-	    newSlots[slots.length] = index;
-	    itemStackSideSlots[sideIndex] = newSlots;
+	@Override
+	public ItemStack getStackInSlotOnClosing(int slot) {
+		ItemStack stack = getStackInSlot(slot);
+		if (stack != null) {
+			setInventorySlotContents(slot, null);
+		}
+		return stack;
 	}
-    }
 
-    @Override
-    public int getSizeInventory() {
-	return itemStacks.size();
-    }
+	@Override
+	public void setInventorySlotContents(int slotIndex, ItemStack stack) {
+		if (slotIndex > getSizeInventory())
+			return;
 
-    @Override
-    public ItemStack getStackInSlot(int i) {
-	MachineItemStack machineStack = getMachineStack(i);
-	if (machineStack == null)
-	    return null;
-	return machineStack.stack;
-    }
-
-    private MachineItemStack getMachineStack(int i) {
-	if (i < 0 || i >= this.itemStacks.size())
-	    return null;
-	return this.itemStacks.get(i);
-    }
-
-    @Override
-    public ItemStack decrStackSize(int i, int j) {
-	MachineItemStack machineStack = getMachineStack(i);
-	if (machineStack == null)
-	    return null;
-
-	ItemStack stack = machineStack.stack;
-	if (stack == null)
-	    return null;
-
-	if (stack.stackSize > j) {
-	    stack = stack.splitStack(j);
-	    onInventoryChanged();
-	    return stack;
+		MachineItemStack machineStack = getMachineStack(slotIndex);
+		if (machineStack != null) {
+			machineStack.stack = stack;
+			if (stack != null && stack.stackSize > getInventoryStackLimit()) {
+				stack.stackSize = getInventoryStackLimit();
+			}
+			onInventoryChanged();
+		}
 	}
-	machineStack.stack = null;
-	onInventoryChanged();
-	return stack;
-    }
 
-    @Override
-    public ItemStack getStackInSlotOnClosing(int slot) {
-	ItemStack stack = getStackInSlot(slot);
-	if (stack != null) {
-	    setInventorySlotContents(slot, null);
+	@Override
+	public String getInvName() {
+		return "Filter";
 	}
-	return stack;
-    }
 
-    @Override
-    public void setInventorySlotContents(int slotIndex, ItemStack stack) {
-	if (slotIndex > getSizeInventory())
-	    return;
-
-	MachineItemStack machineStack = getMachineStack(slotIndex);
-	if (machineStack != null) {
-	    machineStack.stack = stack;
-	    if (stack != null && stack.stackSize > getInventoryStackLimit()) {
-		stack.stackSize = getInventoryStackLimit();
-	    }
-	    onInventoryChanged();
+	@Override
+	public boolean isInvNameLocalized() {
+		return false;
 	}
-    }
 
-    @Override
-    public String getInvName() {
-	return "Filter";
-    }
-
-    @Override
-    public boolean isInvNameLocalized() {
-	return false;
-    }
-
-    @Override
-    public int getInventoryStackLimit() {
-	return 64;
-    }
-
-    @Override
-    public boolean isUseableByPlayer(EntityPlayer entityplayer) {
-	return worldObj.getBlockTileEntity(xCoord, yCoord, zCoord) == this && entityplayer.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) < 64;
-    }
-
-    @Override
-    public void openChest() {
-    }
-
-    @Override
-    public void closeChest() {
-    }
-
-    @Override
-    public boolean isItemValidForSlot(int i, ItemStack itemstack) {
-	MachineItemStack stack = getMachineStack(i);
-	if (stack == null || itemstack == null)
-	    return false;
-	return stack.input && isValidInput(i, itemstack.itemID);
-    }
-
-    protected abstract boolean isValidInput(int slot, int itemID);
-
-    @Override
-    public int[] getAccessibleSlotsFromSide(int var1) {
-	LocalDirection localFrom = DirectionUtils.GetLocalDirection(var1, getForwardDirection());
-	return itemStackSideSlots[localFrom.ordinal()];
-    }
-
-    @Override
-    public boolean canInsertItem(int slotIndex, ItemStack itemstack, int amount) {
-	MachineItemStack machineStack = getMachineStack(slotIndex);
-	if (machineStack != null && machineStack.input) {
-	    return isItemValidForSlot(slotIndex, itemstack);
+	@Override
+	public int getInventoryStackLimit() {
+		return 64;
 	}
-	return false;
-    }
 
-    @Override
-    public boolean canExtractItem(int slotIndex, ItemStack itemstack, int amount) {
-	MachineItemStack machineStack = getMachineStack(slotIndex);
-	if (machineStack != null && machineStack.output) {
-	    return true;
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
+		return worldObj.getBlockTileEntity(xCoord, yCoord, zCoord) == this && entityplayer.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) < 64;
 	}
-	return false;
-    }
+
+	@Override
+	public void openChest() {
+	}
+
+	@Override
+	public void closeChest() {
+	}
+
+	@Override
+	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
+		MachineItemStack stack = getMachineStack(i);
+		if (stack == null || itemstack == null)
+			return false;
+		return stack.input && isValidInput(i, itemstack.itemID);
+	}
+
+	protected abstract boolean isValidInput(int slot, int itemID);
+
+	@Override
+	public int[] getAccessibleSlotsFromSide(int var1) {
+		LocalDirection localFrom = DirectionUtils.GetLocalDirection(var1, getForwardDirection());
+		return itemStackSideSlots[localFrom.ordinal()];
+	}
+
+	@Override
+	public boolean canInsertItem(int slotIndex, ItemStack itemstack, int amount) {
+		MachineItemStack machineStack = getMachineStack(slotIndex);
+		if (machineStack != null && machineStack.input) {
+			if (machineStack.stack == null) {
+				return isItemValidForSlot(slotIndex, itemstack);
+			} else if(machineStack.stack.stackSize + amount <= 64){
+				return isItemValidForSlot(slotIndex, itemstack);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean canExtractItem(int slotIndex, ItemStack itemstack, int amount) {
+		MachineItemStack machineStack = getMachineStack(slotIndex);
+		if (machineStack != null && machineStack.output) {
+			return true;
+		}
+		return false;
+	}
 
 }
--- src/minecraft/ip/industrialProcessing/machines/blastFurnace/TileEntityBlastFurnace.java
+++ src/minecraft/ip/industrialProcessing/machines/blastFurnace/TileEntityBlastFurnace.java
@@ -1,6 +1,9 @@
 package ip.industrialProcessing.machines.blastFurnace;
 
+import ip.industrialProcessing.IndustrialProcessing;
 import ip.industrialProcessing.LocalDirection;
+import ip.industrialProcessing.machines.MachineItemStack;
+import ip.industrialProcessing.machines.TileEntityMachine;
 import ip.industrialProcessing.machines.TileEntityPoweredFluidWorkerMachine;
 import ip.industrialProcessing.machines.crusher.RecipesCrusher;
 import ip.industrialProcessing.recipes.Recipe;
@@ -12,6 +15,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -31,8 +35,8 @@ public class TileEntityBlastFurnace extends TileEntityPoweredFluidWorkerMachine 
 				LocalDirection[] nodirections = new LocalDirection[0];
 
 				this.addStack(null, LocalDirection.UP, true, false);
-				this.addStack(null, nodirections, true, false);
-				this.addStack(null, nodirections, true, false);
+				this.addStack(null, LocalDirection.UP, true, false);
+				this.addStack(null, LocalDirection.UP, true, false);
 
 				addStack(null, nodirections, true, false);
 				addStack(null, nodirections, false, true);
@@ -41,18 +45,60 @@ public class TileEntityBlastFurnace extends TileEntityPoweredFluidWorkerMachine 
 
 				addTank(FluidContainerRegistry.BUCKET_VOLUME * 50, LocalDirection.RIGHT, false, true);
 				addTank(FluidContainerRegistry.BUCKET_VOLUME * 50, LocalDirection.DOWN, false, true);
-				addTank(FluidContainerRegistry.BUCKET_VOLUME * 50, nodirections, false, true);
-			} else {
+				addTank(FluidContainerRegistry.BUCKET_VOLUME * 50, LocalDirection.BACK, false, true);
 
+			} else {
+				int meta = worldObj.getBlockMetadata(xCoord, yCoord - 1, zCoord);
+				worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, meta, 0);
+
+				LocalDirection[] nodirections = new LocalDirection[0];
+
+				this.addStack(null, LocalDirection.BACK, true, false);
+				this.addStack(null, LocalDirection.RIGHT, true, false);
+				this.addStack(null, LocalDirection.LEFT, true, false);
+
+				addStack(null, nodirections, true, false);
+				addStack(null, nodirections, false, true);
+				addStack(null, nodirections, true, false);
+				addStack(null, nodirections, false, true);
 			}
+			worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, IndustrialProcessing.blockBlastFurnace.blockID);
 			init = false;
 		}
 		if (!isDummyBlock) {
 			getBucketFromTank(3, 4, 0);
 			getBucketFromTank(5, 6, 1);
 			super.updateEntity();
+		} else {
+			pushStackDown(0);
+			pushStackDown(1);
+			pushStackDown(2);
 		}
 	};
+
+	private boolean pushStackDown(int slot) {
+		ItemStack stack = getStackInSlot(slot);
+		if (stack == null)
+			return false;
+		TileEntityBlastFurnace te = (TileEntityBlastFurnace) worldObj.getBlockTileEntity(xCoord, yCoord - 1, zCoord);
+		int[] slots = te.getAccessibleSlotsFromSide(ForgeDirection.UP.ordinal());
+		for (int i = 0; i < slots.length; i++) {
+			if (te.canInsertItem(slots[i], stack, stack.stackSize)) {
+				if (te.getStackInSlot(slots[i]) != null) {
+						te.setInventorySlotContents(slots[i], new ItemStack(stack.getItem(), stack.stackSize + te.getStackInSlot(slots[i]).stackSize));
+						te.onInventoryChanged();
+						decrStackSize(slot, stack.stackSize);
+						onInventoryChanged();
+				} else {
+					te.setInventorySlotContents(slots[i], new ItemStack(stack.getItem(), stack.stackSize));
+					te.onInventoryChanged();
+					decrStackSize(slot, stack.stackSize);
+					onInventoryChanged();
+				}
+			}
+		}
+		return false;
+	}
 
 	@Override
 	public boolean hasWork() {
@@ -96,6 +142,7 @@ public class TileEntityBlastFurnace extends TileEntityPoweredFluidWorkerMachine 
 		}
 		return false;
 	}
+
 	public boolean hasDummyBlock(World world, int x, int y, int z) {
 		TileEntity entity = world.getBlockTileEntity(x, y + 1, z);
 		if (entity != null && entity instanceof TileEntityBlastFurnace && ((TileEntityBlastFurnace) entity).isDummyBlock(world, x, y + 1, z)) {
@@ -103,5 +150,22 @@ public class TileEntityBlastFurnace extends TileEntityPoweredFluidWorkerMachine 
 		}
 		return false;
 	}
-
+    @Override
+    public boolean canInsertItem(int slotIndex, ItemStack itemstack, int amount) {
+    if (isDummyBlock){
+    	if (!((TileEntityMachine) worldObj.getBlockTileEntity(xCoord, yCoord-1, zCoord)).canInsertItem(slotIndex,itemstack,amount))
+    		return false;
+    	else
+    		return true;
+    }else{
+		MachineItemStack machineStack = getMachineStack(slotIndex);
+		if (machineStack != null && machineStack.input) {
+			if (machineStack.stack == null) {
+				return isItemValidForSlot(slotIndex, itemstack);
+			} else if(machineStack.stack.stackSize + amount <= 64){
+				return isItemValidForSlot(slotIndex, itemstack);
+			}
+		}
+		return false;
+    }}
 }
