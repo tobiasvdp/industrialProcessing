88164ceeb0bc7cd902d7842069bc4f6fe2dd15f0
Tobias VdP <tobiasvdp@outlook.com>
2013-08-30 19:24:07 +0200

    Started rebuild of the gui for multiblocks

---
+++ src/minecraft/ip/industrialProcessing/multiblock/gui/CTmultiblock.java
@@ -0,0 +1,11 @@
+package ip.industrialProcessing.multiblock.gui;
+
+import java.awt.Container;
+
+public class CTmultiblock extends Container{
+
+	public CTmultiblock() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/minecraft/ip/industrialProcessing/multiblock/gui/GUImultiblock.java
@@ -0,0 +1,17 @@
+package ip.industrialProcessing.multiblock.gui;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+
+public class GUImultiblock extends GuiContainer{
+
+	public GUImultiblock() {
+		super(null);
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
