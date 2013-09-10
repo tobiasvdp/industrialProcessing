d44373f40e7e1edec99ae84851106ec44aed68a6
Tobias VdP <tobiasvdp@outlook.com>
2013-09-10 22:04:32 +0200

    added logic for conveyor belt

--- src/minecraft/ip/industrialProcessing/transport/items/conveyorOutput/TEtransportConveyorBeltOutput.java
+++ src/minecraft/ip/industrialProcessing/transport/items/conveyorOutput/TEtransportConveyorBeltOutput.java
@@ -1,8 +1,10 @@
 package ip.industrialProcessing.transport.items.conveyorOutput;
 
 import ip.industrialProcessing.machines.TileEntitySynced;
+import ip.industrialProcessing.transport.items.interfaces.IItemAcceptor;
+import ip.industrialProcessing.transport.items.interfaces.IItemAcceptoroutput;
 
-public class TEtransportConveyorBeltOutput extends TileEntitySynced{
+public class TEtransportConveyorBeltOutput extends TileEntitySynced implements IItemAcceptoroutput{
 
 	public TEtransportConveyorBeltOutput() {
 		// TODO Auto-generated constructor stub
--- src/minecraft/ip/industrialProcessing/transport/items/interfaces/IItemTransport.java
+++ src/minecraft/ip/industrialProcessing/transport/items/interfaces/IItemTransport.java
@@ -3,8 +3,9 @@ package ip.industrialProcessing.transport.items.interfaces;
 import net.minecraftforge.common.ForgeDirection;
 
 public interface IItemTransport {
-	public void setOutputDirection();
-	public void setInputsDirection();
-	public ForgeDirection getOutputDirection();
+	public void setOutputDirection(ForgeDirection dir, boolean isValid);
+	public void setInputDirection(ForgeDirection dir, boolean isValid);
+	public ForgeDirection[] getOutputDirections();
 	public ForgeDirection[] getInputDirections();
+	public void updatePath();
 }
