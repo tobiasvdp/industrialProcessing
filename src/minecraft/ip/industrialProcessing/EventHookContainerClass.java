88aca2ac0620c28e586064a8fac339d7ec2bf310
tobiasvdp <tobiasvdp@outlook.com>
2013-09-21 09:34:50 +0100

    sync

--- src/minecraft/ip/industrialProcessing/EventHookContainerClass.java
+++ src/minecraft/ip/industrialProcessing/EventHookContainerClass.java
@@ -1,5 +1,6 @@
 package ip.industrialProcessing;
 
+import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
@@ -9,12 +10,30 @@ public class EventHookContainerClass {
 	public void blockPlaced(PlayerInteractEvent event){
 		if(event.action == Action.RIGHT_CLICK_BLOCK){
 			System.out.println("block");
-			System.out.println(event.entityPlayer.worldObj.getBlockTileEntity(event.x, event.y, event.z));
-			System.out.println(event.face);
+			ForgeDirection dir = ForgeDirection.getOrientation(transformToForgeDirection(event.face));
+			System.out.println(event.entityPlayer.worldObj.getBlockTileEntity(event.x+dir.offsetX, event.y+dir.offsetY, event.z+dir.offsetZ));
 		}
 		if(event.action == Action.RIGHT_CLICK_AIR){
 			System.out.println("air");
 			System.out.println(event.entityPlayer.worldObj.getBlockTileEntity(event.x, event.y, event.z));
 		}
+	}
+	
+	private int transformToForgeDirection(int blockMetadata) {
+		switch (blockMetadata) {
+		case 0:
+			return 0;
+		case 1:
+			return 1;
+		case 2:
+			return 3;
+		case 3:
+			return 2;
+		case 4:
+			return 5;
+		case 5:
+			return 4;
+		}
+		return 0;
 	}
 }
