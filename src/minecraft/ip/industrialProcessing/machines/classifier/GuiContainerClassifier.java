ea8e39c06564c742b5a601979c2cb8b82e18df46
Stijn Tallon <stijn_tallon@msn.com>
2013-09-24 14:32:28 +0200

    Fixed classifier GuiContainer with power and worker progress bars

--- src/minecraft/ip/industrialProcessing/machines/classifier/GuiContainerClassifier.java
+++ src/minecraft/ip/industrialProcessing/machines/classifier/GuiContainerClassifier.java
@@ -1,12 +1,17 @@
 package ip.industrialProcessing.machines.classifier;
 
+import ip.industrialProcessing.machines.containers.gui.GuiContainerPoweredFluidWorkerMachine;
+import net.minecraft.entity.player.InventoryPlayer;
+
 import org.lwjgl.util.Point;
 import org.lwjgl.util.Rectangle;
 
-import ip.industrialProcessing.machines.containers.gui.GuiContainerFluidMachine;
-import net.minecraft.entity.player.InventoryPlayer;
+public class GuiContainerClassifier extends GuiContainerPoweredFluidWorkerMachine {
 
-public class GuiContainerClassifier extends GuiContainerFluidMachine {
+	private static final Rectangle PROGRESSBAR_SOURCE = new Rectangle(176, 0, 22, 18);
+	private static final Point PROGRESSBAR_LOCATION = new Point(76, 34);
+	private static final Rectangle PROGRESSBAR_POWER_SOURCE = new Rectangle(200, 0, 52, 8);
+	private static final Point PROGRESSBAR_POWER_LOCATION = new Point(117, 7);
 
 	private static final Rectangle TANK_SOURCE = new Rectangle(176, 17, 16, 50);
 
@@ -21,6 +26,9 @@ public class GuiContainerClassifier extends GuiContainerFluidMachine {
 
 		drawProgressBarTank(TANK_SOURCE, new Point(8, 19), 0);
 		drawProgressBarTank(TANK_SOURCE, new Point(152, 19), 1);
+
+		drawProgressBarWorker(PROGRESSBAR_SOURCE, PROGRESSBAR_LOCATION, true, 0);
+		drawProgressBarPower(PROGRESSBAR_POWER_SOURCE, PROGRESSBAR_POWER_LOCATION, true, 0);
 	}
 
 }
