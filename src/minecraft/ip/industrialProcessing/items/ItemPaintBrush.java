7176a718f16bed5ad491987a6858604f617c3df5
Tobias VdP <tobiasvdp@outlook.com>
2013-10-06 11:57:03 +0200

    Wool gets recoloured!!!!!

--- src/minecraft/ip/industrialProcessing/items/ItemPaintBrush.java
+++ src/minecraft/ip/industrialProcessing/items/ItemPaintBrush.java
@@ -2,10 +2,12 @@ package ip.industrialProcessing.items;
 
 import java.util.List;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import ip.industrialProcessing.IndustrialProcessing;
 
 public class ItemPaintBrush extends ItemDamage {
@@ -17,7 +19,10 @@ public class ItemPaintBrush extends ItemDamage {
 
 	@Override
 	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
-		System.out.println("Item used " + color);
+		int blockId = par3World.getBlockId(par4, par5, par6);
+		if(blockId != 0){
+			Block.blocksList[blockId].recolourBlock(par3World, par4, par5, par6, ForgeDirection.getOrientation(par7), color);
+		}
 		return super.onItemUse(par1ItemStack, par2EntityPlayer, par3World, par4, par5, par6, par7, par8, par9, par10);
 	}
 }
