556fa0db384fba9644db1873998c119451d860d1
Stijn Tallon <stijn_tallon@msn.com>
2013-10-06 20:17:46 +0200

    Tree preparations

---
+++ src/minecraft/ip/industrialProcessing/decoration/trees/BlockIndustrialLeaves.java
@@ -0,0 +1,12 @@
+package ip.industrialProcessing.decoration.trees;
+
+import net.minecraft.block.BlockLeaves;
+
+public class BlockIndustrialLeaves extends BlockLeaves {
+
+	protected BlockIndustrialLeaves(int par1) {
+		super(par1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
