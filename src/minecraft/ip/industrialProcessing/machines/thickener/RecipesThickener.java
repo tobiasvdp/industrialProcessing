22fd77f226bc3bc79d272430d8f123c0566add3a
Tobias VdP <tobiasvdp@outlook.com>
2013-11-26 21:58:38 +0100

    added recipes for the classifier

--- src/minecraft/ip/industrialProcessing/machines/thickener/RecipesThickener.java
+++ src/minecraft/ip/industrialProcessing/machines/thickener/RecipesThickener.java
@@ -3,5 +3,12 @@ package ip.industrialProcessing.machines.thickener;
 import ip.industrialProcessing.machines.RecipesMachine;
 
 public class RecipesThickener extends RecipesMachine {
+	public RecipesThickener() {
+		addDefaultRecipes();
+	}
 
+	private void addDefaultRecipes() {
+		// TODO Auto-generated method stub
+		
+	}
 }
