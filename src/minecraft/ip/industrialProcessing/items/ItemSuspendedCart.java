2bd267c4a825ae4d0a1b360e9fd929669a8c3668
Tobias VdP <tobiasvdp@outlook.com>
2013-12-10 22:18:30 +0100

    added turns

--- src/minecraft/ip/industrialProcessing/items/ItemSuspendedCart.java
+++ src/minecraft/ip/industrialProcessing/items/ItemSuspendedCart.java
@@ -1,5 +1,7 @@
 package ip.industrialProcessing.items;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import ip.industrialProcessing.IndustrialProcessing;
 import ip.industrialProcessing.config.INamepace;
 import ip.industrialProcessing.transport.steve.railway.suspended.cart.EntityFloatingCart;
@@ -21,7 +23,8 @@ public class ItemSuspendedCart extends Item{
 	@Override
 	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
 		if(par3World.getBlockId(par4, par5, par6) == IndustrialProcessing.blockSuspendedRail.blockID ){
-			par3World.spawnEntityInWorld(new EntityFloatingCart(par3World, par4+0.5, par5-0.5, par6+0.5));
+			
+			par3World.spawnEntityInWorld(new EntityFloatingCart(par3World, par4+0.5, par5-0.5, par6+0.5,RandomStringUtils.randomAscii(16)));
 		}
 		return super.onItemUse(par1ItemStack, par2EntityPlayer, par3World, par4, par5, par6, par7, par8, par9, par10);
 	}
--- src/minecraft/ip/industrialProcessing/transport/steve/railway/suspended/cart/EntityFloatingCart.java
+++ src/minecraft/ip/industrialProcessing/transport/steve/railway/suspended/cart/EntityFloatingCart.java
@@ -8,40 +8,49 @@ import net.minecraft.world.World;
 
 public class EntityFloatingCart extends Entity {
 
+	private double speedX = 0;
+	private double speedY = 0;
+	private double speedZ = 0;
 
-	public EntityPlayer getRider(){
+	public EntityPlayer getRider() {
 		return (EntityPlayer) this.worldObj.getPlayerEntityByName(dataWatcher.getWatchableObjectString(17));
 	}
-	public void setRider(EntityPlayer player){
+
+	public void setRider(EntityPlayer player) {
 		this.dataWatcher.updateObject(17, player.username);
 	}
-	public boolean getOccupied(){
-		return  dataWatcher.getWatchableObjectInt(18) == 1;
+
+	public boolean getOccupied() {
+		return dataWatcher.getWatchableObjectInt(18) == 1;
 	}
-	public void setOccupied(boolean val){
-		if(val)
+
+	public void setOccupied(boolean val) {
+		if (val)
 			this.dataWatcher.updateObject(18, 1);
 		else
 			this.dataWatcher.updateObject(18, 0);
 	}
+
 	public EntityFloatingCart(World par1World) {
 		super(par1World);
 		this.setSize(1.0f, 1.0f);
 	}
 
-	public EntityFloatingCart(World world, double d, double y, double e) {
+	public EntityFloatingCart(World world, double d, double y, double e, String name) {
 		super(world);
 		this.setSize(1.2f, 2.0f);
 		this.posX = d;
 		this.posY = y;
 		this.posZ = e;
 		this.boundingBox.setBounds(d, y, e, d + 1, y + 1, e + 1);
+		setName(name);
 	}
 
 	@Override
 	protected void entityInit() {
 		this.getDataWatcher().addObject(17, "");
 		this.getDataWatcher().addObject(18, Integer.valueOf(0));
+		this.getDataWatcher().addObject(19, "");
 	}
 
 	@Override
@@ -77,12 +86,85 @@ public class EntityFloatingCart extends Entity {
 	@Override
 	public void onUpdate() {
 		if (getOccupied()) {
+			moveForward();
 			EntityPlayer player = getRider();
 			player.setPosition(this.posX, this.posY, this.posZ);
 			player.setVelocity(0, 0, 0);
+			this.boundingBox.setBounds(this.posX, this.posY, this.posZ, this.posX + 1, this.posY + 1, this.posZ + 1);
+		} else {
 		}
-		//System.out.println(worldObj.toString() + " " + getOccupied());
 		super.onUpdate();
+	}
+
+	private void moveForward() {
+		int meta = this.worldObj.getBlockMetadata((int) this.posX, (int) this.posY + 1, (int) this.posZ);
+		switch (meta) {
+		case 1:
+			this.posX += speedX;
+			this.posZ = (int) this.posZ + 0.5;
+			if (speedX > 0 && speedX < 0.1)
+				speedX += 0.001;
+			if (speedX < 0 && speedX > -0.1)
+				speedX -= 0.001;
+			if (speedX == 0)
+				speedX += 0.001;
+			speedZ = 0;
+			break;
+		case 2:
+			this.posZ += speedZ;
+			this.posX = (int) this.posX + 0.5;
+			if (speedZ > 0 && speedZ < 0.1)
+				speedZ += 0.001;
+			if (speedZ < 0 && speedZ > -0.1)
+				speedZ -= 0.001;
+			if (speedZ == 0)
+				speedZ += 0.001;
+			speedX = 0;
+			break;
+		case 4:
+			if (speedX > 0) {
+				this.posX += 0.1;
+				this.posZ -= 0.1;
+				this.speedX = 0.1;
+				this.speedZ = -0.1;
+			}else{
+				this.posX -= 0.1;
+				this.posZ += 0.1;
+				this.speedX = -0.1;
+				this.speedZ = +0.1;
+			}
+
+			break;
+			
+		case 5:
+			if (speedZ < 0) {
+				this.posX -= 0.1;
+				this.posZ -= 0.1;
+				this.speedX = -0.1;
+				this.speedZ = -0.1;
+			}else{
+				this.posX += 0.1;
+				this.posZ += 0.1;
+				this.speedX = +0.1;
+				this.speedZ = +0.1;
+			}
+			
+		case 3:
+			if (speedX < 0) {
+				this.posX -= 0.1;
+				this.posZ -= 0.1;
+				this.speedX = -0.1;
+				this.speedZ = -0.1;
+			}else{
+				this.posX += 0.1;
+				this.posZ += 0.1;
+				this.speedX = +0.1;
+				this.speedZ = +0.1;
+			}
+
+			break;
+		default:
+		}
 	}
 
 	public boolean interact(EntityPlayer player) {
@@ -93,6 +175,19 @@ public class EntityFloatingCart extends Entity {
 			setRider(player);
 		}
 		return true;
+	}
+
+	@Override
+	public String getEntityName() {
+		return "EntityFloatingCart." + getName();
+	}
+
+	private String getName() {
+		return dataWatcher.getWatchableObjectString(19);
+	}
+
+	private void setName(String name) {
+		this.dataWatcher.updateObject(19, name);
 	}
 
 }
