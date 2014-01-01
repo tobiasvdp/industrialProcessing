4538a795b6ee094bce533a28dcaa14c7f0d73754
Tobias VdP <tobiasvdp@outlook.com>
2014-01-01 16:45:18 +0100

    added some tags

--- src/minecraft/ip/industrialProcessing/utils/registry/BlockType.java
+++ src/minecraft/ip/industrialProcessing/utils/registry/BlockType.java
@@ -1,5 +1,5 @@
 package ip.industrialProcessing.utils.registry;
 
 public enum BlockType {
-	Block, Machine, Grinding;
+	Block, Machine, Tier0,Tier1,Power,Refinary,Smelting,Ore_Processing, assemble, Dummy,Transport;
 }
