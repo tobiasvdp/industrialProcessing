f63f88e7a5a250bc53d337fe3e5a43c957f8f45d
Stino <stijn_tallon@msn.com>
2014-01-28 17:23:21 +0100

    Profiler fixed in rendererBlock

--- src/minecraft/ip/industrialProcessing/api/rendering/RendererBlock.java
+++ src/minecraft/ip/industrialProcessing/api/rendering/RendererBlock.java
@@ -10,49 +10,50 @@ import net.minecraft.world.IBlockAccess;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class RendererBlock implements ISimpleBlockRenderingHandler {
-	final int renderID;
-	private ModelBlock model;
+    final int renderID;
+    private ModelBlock model;
 
-	public RendererBlock(int renderID, ModelBlock model) {
-		this.renderID = renderID;
-		this.model = model; 
+    public RendererBlock(int renderID, ModelBlock model) {
+	this.renderID = renderID;
+	this.model = model;
+    }
+
+    @Override
+    public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
+	model.renderInventory(block, metadata, modelID, renderer);
+    }
+
+    @Override
+    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
+
+	if (block == null || world == null) {
+	    return false;
 	}
+	Minecraft mc = Minecraft.getMinecraft();
+	mc.mcProfiler.startSection("IP");
+	mc.mcProfiler.startSection(block.getClass().getSimpleName());
+	renderer.overrideBlockTexture = block.getBlockTexture(world, x, y, z, 0);
 
-	@Override
-	public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
-		model.renderInventory(block, metadata, modelID, renderer);
-	}
+	renderer.setRenderBounds(0.0F, 0.0F, 0.0F, 1.0F, 1F, 1.0F);
 
-	@Override
-	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
+	WorldReference reference = new WorldReference(world, x, y, z, block);
+	boolean result = model.renderWorldBlock(reference, modelId, renderer);
 
-		if (block == null || world == null) {
-			return false;
-		}
-	    Minecraft mc = Minecraft.getMinecraft();
-	    mc.mcProfiler.startSection("IP");
-	    mc.mcProfiler.startSection(block.getUnlocalizedName());
-		renderer.overrideBlockTexture = block.getBlockTexture(world, x, y, z, 0);
+	renderer.clearOverrideBlockTexture();
+	mc.mcProfiler.endSection();
+	mc.mcProfiler.endSection(); 
 
-		renderer.setRenderBounds(0.0F, 0.0F, 0.0F, 1.0F, 1F, 1.0F);
+	return result;
+    }
 
-		WorldReference reference = new WorldReference(world, x, y, z, block);
-		boolean result = model.renderWorldBlock(reference, modelId, renderer);
+    @Override
+    public boolean shouldRender3DInInventory() {
+	return true;
+    }
 
-		renderer.clearOverrideBlockTexture();
-		mc.mcProfiler.endSection();
-		mc.mcProfiler.endSection();
-		return result;
-	}
-
-	@Override
-	public boolean shouldRender3DInInventory() {
-		return true;
-	}
-
-	@Override
-	public int getRenderId() {
-		return renderID;
-	}
+    @Override
+    public int getRenderId() {
+	return renderID;
+    }
 
 }
