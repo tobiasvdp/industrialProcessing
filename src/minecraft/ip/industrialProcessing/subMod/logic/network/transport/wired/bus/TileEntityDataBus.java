ed1078096bcc8bfb8cae79fae734abd96e6f12d2
tobiasvdp <tobiasvdp@outlook.com>
2014-02-02 22:53:20 +0000

    fixed te creation for logic transport.

---
+++ src/minecraft/ip/industrialProcessing/subMod/logic/network/transport/wired/bus/TileEntityDataBus.java
@@ -0,0 +1,7 @@
+package ip.industrialProcessing.subMod.logic.network.transport.wired.bus;
+
+import ip.industrialProcessing.subMod.logic.network.microblock.TileEntityLogicTransport;
+
+public class TileEntityDataBus extends TileEntityLogicTransport {
+
+}
