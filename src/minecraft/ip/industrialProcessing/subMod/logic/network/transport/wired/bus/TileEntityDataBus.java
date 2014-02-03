556df708239181ab208e5be71583bb3af02ab3e9
Tobias VdP <tobiasvdp@outlook.com>
2014-02-03 19:15:02 +0100

    push missing file for removal

---
+++ src/minecraft/ip/industrialProcessing/subMod/logic/network/transport/wired/bus/TileEntityDataBus.java
@@ -0,0 +1,9 @@
+package ip.industrialProcessing.subMod.logic.network.transport.wired.bus;
+
+import ip.industrialProcessing.subMod.logic.network.microblock.TileEntityLogicTransport;
+
+public class TileEntityDataBus extends TileEntityLogicTransport {
+	public TileEntityDataBus() {
+		System.out.println("You can't stop me!!!");
+	}
+}
