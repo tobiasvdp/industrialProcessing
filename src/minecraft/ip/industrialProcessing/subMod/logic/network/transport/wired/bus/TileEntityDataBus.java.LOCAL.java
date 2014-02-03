b234613a4e11f96c4ce3c662f90ae2650d565531
Tobias VdP <tobiasvdp@outlook.com>
2014-02-03 19:17:37 +0100

    merge fail fix

--- src/minecraft/ip/industrialProcessing/subMod/logic/network/transport/wired/bus/TileEntityDataBus.java.LOCAL.java
+++
@@ -1,9 +0,0 @@
-package ip.industrialProcessing.subMod.logic.network.transport.wired.bus;
-
-import ip.industrialProcessing.subMod.logic.network.microblock.TileEntityLogicTransport;
-
-public class TileEntityDataBus extends TileEntityLogicTransport {
-	public TileEntityDataBus() {
-		System.out.println("You can't stop me!!!");
-	}
-}
