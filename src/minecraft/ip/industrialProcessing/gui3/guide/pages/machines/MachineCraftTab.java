8071394f59ed7c353eec87d71843bd71b0f37a14
Stijn Tallon <stijn_tallon@msn.com>
2014-02-04 12:42:02 +0100

    Verifying container slotnumber vs slotindex fixed

--- src/minecraft/ip/industrialProcessing/gui3/guide/pages/machines/MachineCraftTab.java
+++ src/minecraft/ip/industrialProcessing/gui3/guide/pages/machines/MachineCraftTab.java
@@ -42,122 +42,122 @@ public class MachineCraftTab extends TabPage implements IButtonClickListener<ICr
     private IButtonClickListener<ItemStack> stackClickListener;
 
     public MachineCraftTab(IButtonClickListener<ItemStack> stackClickListener) {
-	this.stackClickListener = stackClickListener;
-	this.header = TextBlock.createText("Craft");
-	this.activeHeader = TextBlock.createText("Craft", 0xffffffff);
-	this.header.margin = this.activeHeader.margin = new Thickness(0, 5, 0, 0);
-	StackPanel stack = new StackPanel();
-	stack.orientation = Orientation.VERTICAL;
-	this.wrapPanel = new WrapPanel();
-	ScrollPanel scroll = new ScrollPanel();
-	scroll.allowHorizontalScroll = false;
-	scroll.allowVerticalScroll = true;
-	scroll.maxHeight = 18 * 2 + 2;
-	scroll.minHeight = 18 * 2 + 2;
-	scroll.content = wrapPanel;
-	stack.addChild(scroll);
-	this.recipeDock = new UserControl();
-	this.recipeDock.width = this.recipeDock.height = Float.NaN;
-	this.recipeDock.margin = new Thickness(7, 0, 0, 0);
-	stack.addChild(recipeDock);
-	this.content = stack;
+        this.stackClickListener = stackClickListener;
+        this.header = TextBlock.createText("Craft");
+        this.activeHeader = TextBlock.createText("Craft", 0xffffffff);
+        this.header.margin = this.activeHeader.margin = new Thickness(0, 5, 0, 0);
+        StackPanel stack = new StackPanel();
+        stack.orientation = Orientation.VERTICAL;
+        this.wrapPanel = new WrapPanel();
+        ScrollPanel scroll = new ScrollPanel();
+        scroll.allowHorizontalScroll = false;
+        scroll.allowVerticalScroll = true;
+        scroll.maxHeight = 18 * 2 + 2;
+        scroll.minHeight = 18 * 2 + 2;
+        scroll.content = wrapPanel;
+        stack.addChild(scroll);
+        this.recipeDock = new UserControl();
+        this.recipeDock.width = this.recipeDock.height = Float.NaN;
+        this.recipeDock.margin = new Thickness(7, 0, 0, 0);
+        stack.addChild(recipeDock);
+        this.content = stack;
     }
 
     public void setStack(ItemStack tag) {
-	this.wrapPanel.clear();
-	this.recipeDock.child = null;
-	if (tag != null) {
-	    ICraftBase lastFurnace = addFurnaceRecipes(tag);
-	    ICraftBase lastWorkbench = addWorkbenchRecipes(tag);
-	    ICraftBase lastAnvil = addAnvilRecipes(tag);
-	    ICraftBase lastMachine = addMachineRecipes(tag);
-	    if (lastFurnace != null)
-		this.recipeDock.child = lastFurnace.getGui(stackClickListener);
-	    if (lastWorkbench != null)
-		this.recipeDock.child = lastWorkbench.getGui(stackClickListener);
-	    if (lastAnvil != null)
-		this.recipeDock.child = lastAnvil.getGui(stackClickListener);
-	    if (lastMachine != null)
-		this.recipeDock.child = lastMachine.getGui(stackClickListener);
-	}
+        this.wrapPanel.clear();
+        this.recipeDock.child = null;
+        if (tag != null) {
+            ICraftBase lastFurnace = addFurnaceRecipes(tag);
+            ICraftBase lastWorkbench = addWorkbenchRecipes(tag);
+            ICraftBase lastAnvil = addAnvilRecipes(tag);
+            ICraftBase lastMachine = addMachineRecipes(tag);
+            if (lastFurnace != null)
+                this.recipeDock.child = lastFurnace.getGui(stackClickListener);
+            if (lastWorkbench != null)
+                this.recipeDock.child = lastWorkbench.getGui(stackClickListener);
+            if (lastAnvil != null)
+                this.recipeDock.child = lastAnvil.getGui(stackClickListener);
+            if (lastMachine != null)
+                this.recipeDock.child = lastMachine.getGui(stackClickListener);
+        }
     }
 
     private ICraftBase addMachineRecipes(ItemStack tag) {
-	ICraftBase lastCraft = null;
-	ArrayList<IMachineRecipe> recipes = RecipeRegistry.FindRecipeForOutput(tag);
-	for (IMachineRecipe recipe : recipes) {
-	    Block craftingBlock = RecipeRegistry.getBlockForRecipe(recipe);
-	    SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(craftingBlock));
-	    Button button = new Button<ICraftBase>(slot, lastCraft = new MachineCraft(craftingBlock, recipe));
-	    button.subscribeClick(this);
-	    this.wrapPanel.addChild(button);
-	}
-	return lastCraft;
+        ICraftBase lastCraft = null;
+        ArrayList<IMachineRecipe> recipes = RecipeRegistry.FindRecipeForOutput(tag);
+        for (IMachineRecipe recipe : recipes) {
+            Block craftingBlock = RecipeRegistry.getBlockForRecipe(recipe);
+            SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(craftingBlock));
+            Button button = new Button<ICraftBase>(slot, lastCraft = new MachineCraft(craftingBlock, recipe));
+            button.subscribeClick(this);
+            this.wrapPanel.addChild(button);
+        }
+        return lastCraft;
     }
 
     private ICraftBase addAnvilRecipes(ItemStack tag) {
-	ICraftBase lastCraft = null;
+        ICraftBase lastCraft = null;
 
-	List list = ContainerAnvil.getCraftingManager().getRecipeList();
-	for (int j = 0; j < list.size(); j++) {
-	    Object listItem = list.get(j);
-	    if (listItem instanceof IRecipe) {
-		IRecipe recipe = (IRecipe) listItem;
-		ItemStack output = recipe.getRecipeOutput();
-		if (output != null && output.itemID == tag.itemID) {
-		    SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(IndustrialProcessing.blockAnvil));
-		    Button button = new Button<ICraftBase>(slot, lastCraft = new AnvilCraft(recipe));
-		    button.subscribeClick(this);
-		    this.wrapPanel.addChild(button);
-		}
-	    }
-	}
-	return lastCraft;
+        List list = ContainerAnvil.getCraftingManager().getRecipeList();
+        for (int j = 0; j < list.size(); j++) {
+            Object listItem = list.get(j);
+            if (listItem instanceof IRecipe) {
+                IRecipe recipe = (IRecipe) listItem;
+                ItemStack output = recipe.getRecipeOutput();
+                if (output != null && output.itemID == tag.itemID) {
+                    SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(IndustrialProcessing.blockAnvil));
+                    Button button = new Button<ICraftBase>(slot, lastCraft = new AnvilCraft(recipe));
+                    button.subscribeClick(this);
+                    this.wrapPanel.addChild(button);
+                }
+            }
+        }
+        return lastCraft;
     }
 
     private ICraftBase addWorkbenchRecipes(ItemStack tag) {
-	ICraftBase lastCraft = null;
-	List list = CraftingManager.getInstance().getRecipeList();
-	for (int j = 0; j < list.size(); j++) {
-	    Object listItem = list.get(j);
-	    if (listItem instanceof IRecipe) {
-		IRecipe recipe = (IRecipe) listItem;
-		ItemStack output = recipe.getRecipeOutput();
-		if (output != null && output.itemID == tag.itemID) {
-		    SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(Block.workbench));
-		    Button button = new Button<ICraftBase>(slot, lastCraft = new WorkbenchCraft(recipe));
-		    button.subscribeClick(this);
-		    this.wrapPanel.addChild(button);
-		}
-	    }
-	}
-	return lastCraft;
+        ICraftBase lastCraft = null;
+        List list = CraftingManager.getInstance().getRecipeList();
+        for (int j = 0; j < list.size(); j++) {
+            Object listItem = list.get(j);
+            if (listItem instanceof IRecipe) {
+                IRecipe recipe = (IRecipe) listItem;
+                ItemStack output = recipe.getRecipeOutput();
+                if (output != null && output.itemID == tag.itemID) {
+                    SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(Block.workbench));
+                    Button button = new Button<ICraftBase>(slot, lastCraft = new WorkbenchCraft(recipe));
+                    button.subscribeClick(this);
+                    this.wrapPanel.addChild(button);
+                }
+            }
+        }
+        return lastCraft;
     }
 
     private ICraftBase addFurnaceRecipes(ItemStack tag) {
-	ICraftBase lastCraft = null;
-	Map map = FurnaceRecipes.smelting().getSmeltingList();
-	Set set = map.entrySet();
+        ICraftBase lastCraft = null;
+        Map map = FurnaceRecipes.smelting().getSmeltingList();
+        Set set = map.entrySet();
 
-	Iterator it = map.entrySet().iterator();
-	while (it.hasNext()) {
-	    Map.Entry<Integer, ItemStack> entry = (Entry<Integer, ItemStack>) it.next();
-	    ItemStack stack = entry.getValue();
-	    if (stack != null && stack.itemID == tag.itemID) {
-		SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(Block.furnaceIdle));
-		Button button = new Button<ICraftBase>(slot, lastCraft = new FurnaceCraft(entry.getKey(), entry.getValue()));
-		button.subscribeClick(this);
-		this.wrapPanel.addChild(button);
-	    }
-	}
-	return lastCraft;
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Integer, ItemStack> entry = (Entry<Integer, ItemStack>) it.next();
+            ItemStack stack = entry.getValue();
+            if (stack != null && stack.itemID == tag.itemID) {
+                SlotItemControl slot = SlotItemControl.createItemstack(new ItemStack(Block.furnaceIdle));
+                Button button = new Button<ICraftBase>(slot, lastCraft = new FurnaceCraft(entry.getKey(), entry.getValue()));
+                button.subscribeClick(this);
+                this.wrapPanel.addChild(button);
+            }
+        }
+        return lastCraft;
     }
 
     @Override
     public void buttonClicked(Button button, ICraftBase tag, float mouseX, float mouseY, MouseButton mouseButton) {
-	if (tag != null) {
-	    this.recipeDock.child = tag.getGui(stackClickListener);
-	} else
-	    this.recipeDock.child = null;
+        if (tag != null) {
+            this.recipeDock.child = tag.getGui(stackClickListener);
+        } else
+            this.recipeDock.child = null;
     }
 }
